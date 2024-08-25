3d6df03954252e6cfc12437b6ab3db089bc047e6
Movlar <budik.v@yandex.ru>
2024-08-26 00:18:10 +0300

    workflow dispatch 2

--- tests/test_button.py
+++ tests/test_button.py
@@ -10,9 +10,11 @@ def browser():
     chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
 
+
 def test_button_exist(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
 
 def test_button_exist_2(browser):
     browser.get('https://www.qa-practice.com/elements/button/like_a_button')
